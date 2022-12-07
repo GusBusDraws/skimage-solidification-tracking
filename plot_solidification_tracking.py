17c5f7bd40c1a835f5ff264bbebb6901a7e72839
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2022-12-07 20:03:12 +0100

    Lint source file

--- plot_solidification_tracking.py
+++ plot_solidification_tracking.py
@@ -172,7 +172,7 @@ props_0_df.head()
 # along with its bounding box (bbox) in red.
 
 largest_region_0 = labeled_0 == props_0_df.iloc[0]['label']
-minr, minc, maxr, maxc = [props_0_df.iloc[0][f'bbox-{i}'] for i in range(4)]
+minr, minc, maxr, maxc = (props_0_df.iloc[0][f'bbox-{i}'] for i in range(4))
 fig = px.imshow(largest_region_0, binary_string=True)
 fig.add_shape(
         type='rect', x0=minc, y0=minr, x1=maxc, y1=maxr, line=dict(color='Red'))
@@ -238,6 +238,6 @@ ax.scatter(time_us, interface_y_um)
 c0, c1 = np.polynomial.polynomial.polyfit(time_us, interface_y_um, 1)
 ax.plot(time_us, c1 * time_us + c0, label=f'Velocity: {abs(round(c1, 3))} m/s')
 ax.set_title('S-L interface location vs. time')
-ax.set_ylabel('Location ($\mu$m)')
-ax.set_xlabel('Time ($\mu$s)')
+ax.set_ylabel(r'Location ($\mu$m)')
+ax.set_xlabel(r'Time ($\mu$s)')
 plt.show()
